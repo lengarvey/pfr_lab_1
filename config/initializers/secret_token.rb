74eefa65eb54e00c7fd22f01b4095664ecfcc8fe
Leonard Garvey <lengarvey@gmail.com>
2012-11-05 20:26:12 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GaDemo::Application.config.secret_token = '8f2ef056de948004d369db3e20e3c8dcdd7de28d3cfadb489b3bb2a40ddaedea0ae2fb5572fc037523dcbfb97d3fcecdf30cc99bc5988529ee21ff543c95aa36'
